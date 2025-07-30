c82e5115fe9dfbcfe2903b7f87436f5673b4e9ce
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-07-30 14:23:25 +0100

    add schema for ssh command tool
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- packages/agent/main.ts
+++ packages/agent/main.ts
@@ -3,18 +3,25 @@ import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { tool } from "@langchain/core/tools";
 import { Messages} from "@langchain/langgraph";
 import { execSync } from 'child_process';
+import { z } from "zod";
 
 const llm = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
   maxOutputTokens: 2048,
 });
 
+const sshCommandSchema = z.object({
+  username: z.string().describe("The username to attempt to login as via the SSH connection. Unless otherwise specified, use 'ubuntu'."),
+  sshCommand: z.string().describe("The command to run on the remote server."),
+});
+
 const sshCommandTool = tool(
-  async ( sshCommand: string ) => {
-    return execSync(`ssh -F /opt/machine-id/ssh_config ubuntu@quotes.mwihack.cloud.gravitational.io ${sshCommand}`).toString()
+  async ({ username, sshCommand }) => {
+    return execSync(`ssh -F /opt/machine-id/ssh_config ${username}@quotes.mwihack.cloud.gravitational.io ${sshCommand}`).toString()
   }, {
+    schema: sshCommandSchema,
     name: "run_ssh_command",
-    description: "Runs a command on a remote server via SSH.",
+    description: "Runs a command on a remote server via SSH using the user you specify.",
   }
 )
 
