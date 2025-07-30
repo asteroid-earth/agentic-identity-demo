ddc8451368d8fbf30040f1bc130c017b9b63c118
Nick Marais <nicholas.marais@goteleport.com>
2025-07-30 11:42:17 +0100

    Agent package

---
+++ packages/agent/main.ts
@@ -0,0 +1,10 @@
+export function prompt(params: {
+  prompt: string;
+  roles: string[];
+  assertionToken: string;
+}) {
+  console.log("AGENT: Received prompt", params);
+
+  // TODO: Implement agent.
+  return "--agent response goes here--";
+}
