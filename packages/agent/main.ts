30d615d4c9933a836dd005fd7021f65027fdb2a6
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-07-30 13:47:47 +0100

    add agent integrated to backend
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- packages/agent/main.ts
+++ packages/agent/main.ts
@@ -1,10 +1,40 @@
-export function prompt(params: {
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { tool } from "@langchain/core/tools";
+import { Messages} from "@langchain/langgraph";
+import { execSync } from 'child_process';
+
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  maxOutputTokens: 2048,
+});
+
+const sshCommandTool = tool(
+  async ( sshCommand: string ) => {
+    return execSync(`ssh -F /opt/machine-id/ssh_config ubuntu@quotes.mwihack.cloud.gravitational.io ${sshCommand}`).toString()
+  }, {
+    name: "run_ssh_command",
+    description: "Runs a command on a remote server via SSH.",
+  }
+)
+
+const agent = createReactAgent({
+  llm,
+  tools: [sshCommandTool],
+});
+
+export async function prompt(params: {
   prompt: string;
   roles: string[];
   assertionToken: string;
 }) {
   console.log("AGENT: Received prompt", params);
 
-  // TODO: Implement agent.
-  return "--agent response goes here--";
+  const messages: Messages = [{role: "user", content: params.prompt}];
+
+  const response = await agent.invoke({messages});
+
+  const lastMessageIndex = response.messages.length - 1;
+
+  return response.messages[lastMessageIndex].content
 }
