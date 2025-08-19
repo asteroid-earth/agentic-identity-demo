a9d6e1f5b1d75dc8675d1311c13ab2de65a54864
Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
2025-08-19 03:45:57 -0600

    Add agent (#1)
    * add agent integrated to backend
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add missing await statement to prompt call
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add schema for ssh command tool
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add agent integrated to backend
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add missing await statement to prompt call
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add schema for ssh command tool
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add debug logs
    
    * add app access tool
    
    * updates from server to make it work
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    * add .idea configs to gitignore
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dave Sudia <15764229+thedevelopnik@users.noreply.github.com>

--- packages/agent/main.ts
+++ packages/agent/main.ts
@@ -1,10 +1,107 @@
-export function prompt(params: {
+import {ChatGoogleGenerativeAI} from "@langchain/google-genai";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { tool } from "@langchain/core/tools";
+import { Messages } from "@langchain/langgraph";
+import { execSync } from 'child_process';
+import { z } from "zod";
+import fetch from "node-fetch";
+import axios from "axios";
+import fs from 'fs/promises';
+
+const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  maxOutputTokens: 2048,
+});
+
+const systemPrompt = "You are a helpful assistant to DevOps and Platform Engineers. When running SSH commands, use the 'ubuntu' user unless otherwise specified.";
+
+const sshCommandSchema = z.object({
+  username: z.string().describe("The username to attempt to login as via the SSH connection. Unless otherwise specified, use 'ubuntu'."),
+  sshCommand: z.string().describe("The command to run on the remote server."),
+});
+
+export async function prompt(params: {
   prompt: string;
   roles: string[];
   assertionToken: string;
 }) {
   console.log("AGENT: Received prompt", params);
 
-  // TODO: Implement agent.
-  return "--agent response goes here--";
+  const logDebug = async (data: any) => {
+    const logEntry = `${new Date().toISOString()}: ${JSON.stringify(data, null, 2)}\n`;
+    await fs.appendFile(`${__dirname}/langgraph-debug.log`, logEntry);
+  };
+
+  const sshCommandTool = tool(
+    async ({ username, sshCommand}) => {
+
+      const body = { roles: params.roles };
+      const response = await fetch("http://127.0.0.1:8080/ssh-config", {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: {
+          "Authorization": `Bearer ${params.assertionToken}`,
+        }
+      });
+
+      const data = await response.json() as { path: string };
+
+      const path = data.path;
+
+      return execSync(`ssh -F ${path} ${username}@quotes.mwihack.cloud.gravitational.io ${sshCommand}`).toString()
+    }, {
+      schema: sshCommandSchema,
+      name: "run_ssh_command",
+      description: "Runs a command on a remote server via SSH using the user you specify.",
+    }
+  )
+
+  const getQuoteTool = tool(
+    async () => {
+      const body = { roles: params.roles, application: "quotes" };
+
+      const socketResponse = await axios({
+        method: "post",
+        url: "http://127.0.0.1:8080/application-tunnel",
+        data: body,
+        headers: {
+          "Authorization": `Bearer ${params.assertionToken}`,
+        }
+      });
+
+      const data = await socketResponse.data as { id: string, address: string, expires: string };
+
+      let socketPath = data.address;
+
+      socketPath = socketPath.replace("unix://", "");
+
+      const quoteResponse = await axios({
+        url: "http://quotes.mwihack.cloud.gravitational.io/api/quotes/random",
+        socketPath,
+        method: "get"
+      });
+
+      logDebug(quoteResponse.data)
+
+      return await quoteResponse.data
+    },
+    {
+      name: "get_quote",
+      description: "Gets a random quote from a remote application, along with the quote's category and status.",
+    }
+  );
+
+  const agent = createReactAgent({
+    llm,
+    tools: [sshCommandTool, getQuoteTool],
+    prompt: systemPrompt,
+  });
+
+  const messages: Messages = [{role: "user", content: params.prompt}];
+
+  const response = await agent.invoke({messages});
+
+  const lastMessageIndex = response.messages.length - 1;
+
+  return response.messages[lastMessageIndex].content
 }
